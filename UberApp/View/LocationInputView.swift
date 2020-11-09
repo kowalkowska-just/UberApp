d3b11cc5eada3527c1685d8d487df732975f68e1
Justyna <kowalkowska.just@gmail.com>
2020-11-09 22:05:25 +0100

    Finished to create Location Input View UI.

--- UberApp/View/LocationInputView.swift
+++ UberApp/View/LocationInputView.swift
@@ -25,6 +25,63 @@ class LocationInputView: UIView {
         return button
     }()
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Justyna Kowalkowska"
+        label.textColor = .darkGray
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    private let startLocationIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    private let linkingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        return view
+    }()
+    
+    private let destinationIndicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private lazy var startingLocationTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Current Location"
+        tf.backgroundColor = UIColor.rgb(red: 223, green: 228, blue: 234)
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.isEnabled = false
+        
+        let paddingView = UIView()
+        paddingView.anchor(width: 8, height: 30)
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        
+        return tf
+    }()
+    
+    private lazy var destinationLocationTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter a destination.."
+        tf.backgroundColor = UIColor.rgb(red: 206, green: 214, blue: 224)
+        tf.returnKeyType = .search
+        tf.font = UIFont.systemFont(ofSize: 14)
+//        tf.isEnabled = false
+        
+        let paddingView = UIView()
+        paddingView.anchor(width: 8, height: 30)
+        tf.leftView = paddingView
+        tf.leftViewMode = .always
+        
+        return tf
+    }()
+    
 //MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -36,6 +93,38 @@ class LocationInputView: UIView {
         addSubview(backButton)
         backButton.anchor(top: topAnchor, left: leftAnchor, paddingTop: 44, paddingLeft: 12, width: 24, height: 25)
         
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: backButton)
+        titleLabel.centerX(inView: self)
+        
+        addSubview(startingLocationTextField)
+        startingLocationTextField.anchor(top: backButton.bottomAnchor, left: leftAnchor,
+                                         right: rightAnchor, paddingTop: 10, paddingLeft: 40,
+                                         paddingRight: 40, height: 30)
+        
+        addSubview(destinationLocationTextField)
+        destinationLocationTextField.anchor(top: startingLocationTextField.bottomAnchor,
+                                            left: leftAnchor, right: rightAnchor,
+                                            paddingTop: 12, paddingLeft: 40, paddingRight: 40,
+                                            height: 30)
+        
+        addSubview(startLocationIndicatorView)
+        startLocationIndicatorView.centerY(inView: startingLocationTextField)
+        startLocationIndicatorView.anchor(left: leftAnchor, paddingLeft: 20,
+                                          width: 6, height: 6)
+        startLocationIndicatorView.layer.cornerRadius = startLocationIndicatorView.bounds.size.width / 2
+        
+        addSubview(destinationIndicatorView)
+        destinationIndicatorView.centerY(inView: destinationLocationTextField)
+        destinationIndicatorView.anchor(left: leftAnchor, paddingLeft: 20,
+                                        width: 6, height: 6)
+        
+        addSubview(linkingView)
+        linkingView.centerX(inView: startLocationIndicatorView)
+        linkingView.anchor(top: startLocationIndicatorView.bottomAnchor,
+                           bottom: destinationIndicatorView.topAnchor,
+                           paddingTop: 4, paddingBottom: 4,
+                           width: 0.5)
     }
     
     required init?(coder: NSCoder) {
